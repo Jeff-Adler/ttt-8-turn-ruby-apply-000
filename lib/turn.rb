949161a4c1f9caccaea04b583e8219a1eba423db
Jeff Adler <jeffm.adler@gmail.com>
2020-05-29 16:42:55 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,26 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board,index)
+ if (index.between?(0,8) && position_taken?(board,index))
+    return TRUE
+  else
+    return FALSE
+end
+
+def position_taken?(board,index)
+ if (board[index] == " " || board[index] == "" || board[index] == nil)
+    return TRUE
+  else
+    return FALSE
+  end
+end
+
+def move(board,index,player = "X")
+  board[index] = player
+end
